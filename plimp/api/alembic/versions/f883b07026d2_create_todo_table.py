5386c9d156c4b9c8819b39cab4c1ee003fcdc3b2
RobinDeSmet <robin_de_smet@hotmail.be>
2025-06-07 18:25:22 +0200

    fix: linting

--- plimp/api/alembic/versions/f883b07026d2_create_todo_table.py
+++ plimp/api/alembic/versions/f883b07026d2_create_todo_table.py
@@ -5,14 +5,12 @@ Revises:
 Create Date: 2025-06-07 18:20:24.965597
 
 """
-from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
+from typing import Sequence, Union
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'f883b07026d2'
+revision: str = "f883b07026d2"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
